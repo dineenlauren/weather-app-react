0042faf26b44dbe92adac109af6e3c93cecfbd66
Dineen <35275339+dineenlauren@users.noreply.github.com>
2022-05-18 12:37:42 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -12,30 +12,42 @@ function App() {
   const [apiData, setApiData] = useState({});
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-  const city = useState('Dallas');
+  const [getCity, setGetCity] = useState('Dallas');
+  const [city, setCity] = useState('Dallas');
+  // const city = useState('Dallas');
 
   // API KEY AND URL
   const apiKey = 'e1d3bf274775b99b6873fda0ede019f4';
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  const weatherUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}https://api.openweathermap.org/data/2.5`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
-    // fetch(`${apiUrl}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${apiKey}`)
+
 
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => setApiData(data));
 
-
-
-
     console.log("Latitude is:", lat)
     console.log("Longitude is:", long)
   }, [apiUrl, lat, long]);
+
+  const inputHandler = (event) => {
+    setGetCity(event.target.value);
+
+  };
+
+  const submitHandler = () => {
+    setCity(getCity);
+  };
+
+  const kelvinToFarenheit = (k) => {
+    return (k - 273.15).toFixed(2);
+  };
 
   const [radioValue, setRadioValue] = useState('1');
   const radios = [
@@ -43,9 +55,7 @@ function App() {
     { name: 'C\u00B0', value: '2' },
   ];
 
-  const kelvinToFarenheit = (k) => {
-    return Math.round(k - 273.15);
-  };
+
 
   return (
     <Container className="App">
@@ -55,31 +65,28 @@ function App() {
           {apiData.name}
         </h1>
       </div>
+
+      <div className="col-auto">
+        <input
+          type="text"
+          id="location-name"
+          className="form-control"
+          onChange={inputHandler}
+          value={getCity}
+        />
+      </div>
+      <button className="btn btn-primary mt-2" onClick={submitHandler}>
+        Search
+      </button>
       {apiData.main ? (
         <Container className="weather-container py-0">
-          <Container className="current-container">
-            <Row className="current-weather py-2">
+          <Container className="current-container pt-5">
+            <Row>
               <Col>
-                <Row>
-                  <Col>
-                    <p>
-                      {' '}
-                      {apiData.weather[0].main}
-                    </p>
-                  </Col>
-                  <Col>
-                    <p className="h2">
-                      {kelvinToFarenheit(apiData.main.temp)}C&deg;
-                    </p>
-                  </Col>
-                  <Col>
-                    <img
-                      src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
-                      alt="weather status icon"
-                      className="weather-icon"
-                    />
-                  </Col>
-                </Row>
+                <p className="h5">
+                  <i className="fas fa-map-marker-alt"></i>{' '}
+                  {apiData.name}
+                </p>
               </Col>
               <Col>
                 <ButtonGroup className="mb-2">
@@ -93,18 +100,39 @@ function App() {
                       value={radio.value}
                       checked={radioValue === radio.value}
                       onChange={(e) => setRadioValue(e.currentTarget.value)}
+
                     >
+
                       {radio.name}
                     </ToggleButton>
                   ))}
                 </ButtonGroup>
               </Col>
+            </Row>
+            <Row className="current-weather py-2">
               <Col>
-                <p className="h5">
-                  <i className="fas fa-map-marker-alt"></i>{' '}
-                  <strong>{apiData.name}</strong>
-                </p>
+                <Row>
+                  <Col>
+                    <p>
+                      {' '}
+                      {apiData.weather[0].main}
+                    </p>
+                  </Col>
+                  <Col>
+                    <p className="h2">
+                      {Math.round(apiData.main.temp)}F&deg;
+                    </p>
+                  </Col>
+                  <Col>
+                    <img
+                      src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
+                      alt="weather status icon"
+                      className="weather-icon"
+                    />
+                  </Col>
+                </Row>
               </Col>
+
               <Col>
               </Col>
             </Row>
